ed14d0367347a9c908110442482706f4c8c8517e
Manikanta Reddy D <manikanta.reddy.d@live.com>
2016-08-23 23:07:58 +0530

    Added add entry method

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+fs = require('fs');
 var app = express();
 
 var path = __dirname + '/views/';
@@ -10,6 +11,20 @@ app.get("/",function(req,res){
 
 app.get("/getData",function(req,res){
     res.sendFile(__dirname + "/data/"+"data.json");
+});
+
+app.get("/modifyEntry",function(req,res){
+  res.send("LUL");
+  var obj=JSON.parse(fs.readFileSync('./data/data.json').toString());
+  console.log(obj);
+  var name = req.query.n;
+  var price = req.query.p;
+  var stock = req.query.s;
+  obj.push({"name":name,"price":price,"stock":stock});
+  var poo= JSON.stringify(obj);
+  fs.writeFileSync('./data/data.json', poo);
+  console.log(obj);
+  console.log(poo);
 });
 
 app.listen(3000,function(){
